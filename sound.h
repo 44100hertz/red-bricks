61a59def5532217e0231b2ac34cd248594951509
Samuel Pagenkopf <ssaammp@gmail.com>
2017-07-15 21:27:25 -0500

    proper prototypes

--- sound.h
+++ sound.h
@@ -1,6 +1,6 @@
 void beep(int pitch);
 void beep_sweep(int pitch, double sweep);
 
-void sound_toggle();
-void sound_init();
+void sound_toggle(void);
+void sound_init(void);
 void sound_level(int level);
